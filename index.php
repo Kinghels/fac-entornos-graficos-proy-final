c868f9b002eac90f4ddf636c9d7aa7c929a349ec
Kevin Inghels <kevin.inghels@gmail.com>
2014-11-28 19:31:31 -0300

    Eliminado prueba dropdown

--- index.php
+++ index.php
@@ -23,18 +23,6 @@
             <div id="titulo"><h1>Videoteca</h1>
             <h2>Sitio De Alquiler de Peliculas Online</h2>
             
-            </div>
-            <div class="dropdown">
-				<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown">
-				    Dropdown
-					<span class="caret"></span>
-				</button>
-				<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-				    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Action</a></li>
-				    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>
-				    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li>
-				    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Separated link</a></li>
-				</ul>
 			</div>
             <div id="logincontainer" class="box info">
 			<?php
